bc522f62688a486021092a6865eefa34e388b377
Esteban Maldonado <oscarmaldonado.02@outlook.com>
2021-04-21 12:42:08 -0500

    Corrigiendo el coversor de dolares

---
+++ conversor-de-dolares-apesos.py
@@ -0,0 +1,7 @@
+dolares = input("¿Cuántos dolares americanos tienes?: ")
+dolares = float(dolares)
+valor_dolar = 3875
+dolares = valor_dolar * dolares
+pesos = round(dolares, 2)
+pesos = str(dolares)
+print("Tienes $" + pesos + " pesos")
