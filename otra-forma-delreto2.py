c9117b0cbbe9031b117f857149cc30b84604f9b1
Esteban Maldonado <oscarmaldonado.02@outlook.com>
2021-06-20 15:05:21 -0500

    Domingo de retos

---
+++ otra-forma-delreto2.py
@@ -0,0 +1,41 @@
+En caso lo quieras hacer con contadores, tendríamos que declara un contador para cada participante, para este caso hare el código sin usar un diccionario
+
+#delcaramos las variables
+fav_alejandro = "RAIFZ"
+fav_carolina = "UZEOF"
+ganadores_carrera = "IHZOWFEI"
+
+#con inputs
+# fav_alejandro = input("Ingrese las favoritas de alejandro: ")
+# fav_carolina = input("Ingrese las favoritas de carolina: ")
+# ganadores_carrera = input("Ingrese los ganadores")
+
+La función para determinar al ganador se quedará igual. Ahora en vez de sumar 1 al valor del diccionario lo haremos al contador del participante, lo mismo para obtener el valor.
+
+#iniciamos los puntos con 0
+points_alejandro = 0
+points_carolina = 0
+
+for g in ganadores_carrera:
+    #si la letra solo esta en alejandro
+    if g in fav_alejandro and g not in fav_carolina:
+        points_alejandro+=1
+        #verifiacmos al ganador
+        print(winner(points_alejandro,points_carolina),end="") #end="" para que se muestre horizontal
+    #si la letra solo esta en carolina
+    elif g in fav_carolina and g not in fav_alejandro:
+        points_carolina+=1
+        print(winner(points_alejandro,points_carolina),end="")
+    #si esta en ambos
+    elif g in fav_alejandro and g in fav_carolina:
+        #sumamos el punto a ambos
+        points_carolina+=1
+        points_alejandro+=1
+        #verifiacmos al ganador
+        print(winner(points_alejandro,points_carolina),end="")
+    #si no esta en ninguno
+    elif g not in fav_alejandro and g not in fav_carolina:
+        #verifiacmos al ganador
+        print(winner(points_alejandro,points_carolina),end="")
+
+utilicé contadores tal y como me pediste en los comentarios, si no deseas utilizar diccionarios y prefieres hacerlo de esta forma no hay problema, solo que esto también se puede resumir usando otra estructura de dato como listas [] o tuplas () o una mezcla de ambos, una lista de tuplas [(participante1,point),(participante2,points)]. Te dejo eso para que juegues y aprendas más :D
--- reto2.py
+++ reto2.py
@@ -1,43 +1,28 @@
-#Primero hacemos una lista de todas las escuderías, para poder así asignar las favoritas de x personas, además de poder asignarles su respectiva inicial
-A = "Alfa Romero"
-B = "BMW Sauber"
-C = "Racing Point"
-E = "Mercedes"
-F = "Ferrari"
-H = "Haas"
-I = "Force India"
-L = "Lotus"
-M = "McLaren"
-N = "Renault"
-O = "Manor"
-P = "Alpine"
-R = "Red Bull"
-S = "Aston Martin"
-T = "Toro Rosso"
-U = "Marussia"
-W = "Williams"
-Z = "AlphaTauri"
+def winner(puntos_a,puntos_c):
+    if puntos_a>puntos_c: 
+        return "A" 
+    elif puntos_a==puntos_c: 
+        return "E" 
+    else: 
+        return "C" 
 
-# Declarar las escuderias favoritas de Alejandro y Carolina Respectivamente.
-escuderias_fav_alejandro = "RAIFZ"
-escuderias_fav_carolina = "UZEOF"
-posibles_ganadoras = "ABCEFHILMNOPRSTUWZ"
+participantes = {"Alejandro":{"fav":input("Ingrese las escuderias favoritas de alejandro: \n"), "puntos":0}, "Carolina":{"fav":input("Ingrese las escuderias favoritas de Carolina: \n"),"puntos":0}}
 
-ingreso_esc_fav_alejandro = input("Ingrese la lista de escuderías favoritas de Alejandro:\n")  
+ganadores_carrera = input("Ingrese los ganadores de la Temporada: ")
 
-ingreso_esc_fav_carolina = input("Ingrese la lista de escuderías favortias de Carolina: \n")
+alejandro = participantes["Alejandro"] 
+carolina = participantes["Carolina"]
 
-ingreso_ganadores_tempo = input("Ingrese los ganadores de la temporada: ")
-
-for i in ingreso_esc_fav_alejandro:
-    for j in ingreso_ganadores_tempo:
-        if(i==j):
-            print("A")
-            break
-for k in ingreso_esc_fav_carolina:
-    for j in ingreso_ganadores_tempo:
-        if(k==j) and i:
-            print("C")
-            break
-    
-
+for g in ganadores_carrera:
+    if g in alejandro["fav"] and g not in carolina["fav"]:
+        alejandro["puntos"]+=1
+        print(winner(alejandro["puntos"],carolina["puntos"]), end="") 
+    elif g in carolina["fav"] and g not in alejandro["fav"]:
+        carolina["puntos"]+=1 
+        print(winner(alejandro["puntos"],carolina["puntos"]), end="")
+    elif g in alejandro["fav"] and g in carolina["fav"]:
+        carolina["puntos"]+=1 
+        alejandro["puntos"]+=1
+        print(winner(alejandro["puntos"],carolina["puntos"]), end="")
+    elif g not in alejandro["fav"] and g not in carolina["fav"]:
+        print(winner(alejandro["puntos"],carolina["puntos"]), end="")
