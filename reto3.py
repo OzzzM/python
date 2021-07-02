57afb3f125d7867e6d42a188223d0787d261f060
Esteban Maldonado <oscarmaldonado.02@outlook.com>
2021-07-02 11:08:14 -0500

    Terminando el reto 3

--- reto3.py
+++ reto3.py
@@ -1,14 +1,25 @@
 
-entradas_user = input("ingrese la lista de viajes realizados separados por un espacio: ").split(" ")
-lista = entradas_user
+entradas_user = input()
+lista = entradas_user.split(" ")
+contador = 1
+cadena_contador = ""
+cadena_palabras = ""
 
-frecuencia = 0
 
-for inicial in lista:
-    if inicial in lista == inicial:
-        frecuencia += 1
+for indice in range((len(lista)-1)):
+    
+    if lista[indice] == lista[indice+1]:     
+        contador += 1 
+    else:
+        cadena_contador = cadena_contador + str(contador) + " "
+        cadena_palabras = cadena_palabras + lista[indice] + " " 
+        contador = 1     
 
-print(frecuencia)
-# print("A: " "\n" ,contadorA)
-# print("B: " "\n" ,contadorB)
+cadena_contador = cadena_contador + str(contador)
+cadena_palabras = cadena_palabras + lista[indice]
+
+print(cadena_palabras)
+print(cadena_contador)
+
+
 
