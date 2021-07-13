f5a329422ac4cab7889e0b334506ff0bc46760bb
Oscar Maldonado <oscarmaldonado.02@gmail.com>
2021-07-12 20:03:28 -0500

    resolviendo la ultima función

--- laminas_marvel.py
+++ laminas_marvel.py
@@ -8,8 +8,27 @@ def lista_clases(clases):
   return contador
 
 def laminas_faltantes_por_clase(indices, clases, clase_a_verificar):
-  #resultado = []
   
-  lista_indices = indices[len(indices.index)]
-  
+  resultado = []
+  for indice in indices:
+    if clases[indice] == clase_a_verificar:
+      resultado.append(indice)
+    
   return resultado
+
+def laminas_faltantes(laminas_persona_1, laminas_persona_2):
+  resultado2 = []
+  
+  for lamina in laminas_persona_1:
+    if lamina not in laminas_persona_2:
+      resultado2.append(lamina)
+
+  return resultado2
+
+def cantidad_laminas_cambiables(laminas_persona_1, laminas_persona_2):
+  resultado3 = 0
+  
+  for laminas in laminas_persona_1:
+    if laminas not in laminas_persona_2:
+      resultado3 = resultado3 + laminas
+  return resultado3
--- test_suma.py
+++ test_suma.py
@@ -1,3 +1,6 @@
 import laminas_marvel
 
-print(laminas_marvel.lista_clases([3,2,1,1,1,3,2,1,1,1]))
+# print(laminas_marvel.lista_clases([3,2,1,1,1,3,2,1,1,1]))
+# print(laminas_marvel.laminas_faltantes_por_clase([1,3,6,8], [3,2,1,1,1,3,2,1,1,1], 2))
+# print(laminas_marvel.laminas_faltantes([3,5,7,10,15,16], [4,10,5,8]))
+print(laminas_marvel.cantidad_laminas_cambiables([3,5,7,10,15,16], [4,10,5,8]))
